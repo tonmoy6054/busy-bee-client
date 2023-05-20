49ea5b24e9a4ff674c273c61476654507d116837
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-05-20 23:59:12 +0600

    fake data made

--- src/Pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/Pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -37,11 +37,16 @@ function ShopByCategory() {
         >
           <h2>Content 1</h2>
           <hr />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-            praesentium incidunt quia aspernatur quasi quidem facilis quo nihil
-            vel voluptatum?
-          </p>
+          <div className="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
         </div>
 
         <div
